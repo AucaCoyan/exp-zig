6459832ec19977164c989934cbad723f0a2443b4
Auca Coyan <aucacoyan@gmail.com>
2024-08-19 20:23:40 -0300

    :sparkles: add some lines

--- hello-world/src/main.zig
+++ hello-world/src/main.zig
@@ -2,18 +2,21 @@ const std = @import("std");
 
 pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    std.debug.print("All your {s} are belong to us.", .{"codebase"});
+    const arr = [_]u8{ 1, 3, 4, 5 };
+    arr.len;
+    std.debug.print("{s}", arr);
 
     // stdout is for the actual output of your application, for example if you
     // are implementing gzip, then only the compressed bytes should be sent to
     // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
+    // const stdout_file = std.io.getStdOut().writer();
+    // var bw = std.io.bufferedWriter(stdout_file);
+    // const stdout = bw.writer();
+    //
+    // try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    //
+    // try bw.flush(); // don't forget to flush!
 }
 
 test "simple test" {
@@ -21,4 +24,8 @@ test "simple test" {
     // defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
     try list.append(42);
     try std.testing.expectEqual(@as(i32, 42), list.pop());
+}
+
+test "always fails" {
+    try std.testing.expect(false);
 }
