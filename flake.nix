15b7117b47da945f8fff6ceedcfa24c776b6a3a9
Auca Coyan <aucacoyan@gmail.com>
2026-03-21 11:46:58 -0300

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Zig development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            zig
+            zls  # Zig Language Server for editor support
+          ];
+
+          shellHook = ''
+            echo "Zig development environment"
+            echo "Zig version: $(zig version)"
+          '';
+        };
+      }
+    );
+}
